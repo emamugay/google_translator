228b7db2e50a67609ee10165ce6c3e27e2816813
Edgar Mamugay <edgar.mamugay@gmail.com>
2019-12-15 18:01:41 +0800

    Removing comment lines

--- translate.py
+++ translate.py
@@ -39,40 +39,6 @@ def run_cmd(cmd):
     return output
 
 def start_new_session():
-    WINDOW_SIZE = "1920,1080"
-
-    #chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument("--ignore-ssl-errors=true")
-    #chrome_options.add_argument("--ssl-protocol=TLSv1")
-    #chrome_options.add_argument("--hide-scrollbars")
-    #chrome_options.add_argument("--disable-gpu")
-    #chrome_options.add_argument("--log-level=3")
-    #chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-    #chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36")
-
-    ##chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--headed")
-    #path = "/opt/"
-    #CHROMEDRIVER_PATH = "/mnt/Devs/Python/scrapy/mmi/driver/geckodriver"
-    ##CHROMEDRIVER_PATH = "/mnt/Devs/Python/scrapy/mmi/driver/chromedriver"
-    ##chrome_options.binary_location = "/usr/bin/google-chrome"
-    #chrome_options.binary_location = "/usr/bin/firefox"
-
-    #chrome_options.add_experimental_option("prefs", {
-    #"download.default_directory": path + "data",
-    #"download.prompt_for_download": False,
-    #"download.directory_upgrade": True,
-    #"safebrowsing.enabled": True
-    #})
-
-    #os.environ["webdriver.chrome.driver"] = CHROMEDRIVER_PATH
-    ##driver = webdriver.Chrome(executable_path = CHROMEDRIVER_PATH, options = chrome_options)
-    #driver = webdriver.Firefox(executable_path = CHROMEDRIVER_PATH, options = chrome_options)
-    #driver.command_executor._commands["send_command"] = ("POST", '/session/' + driver.session_id + '/chromium/send_command')
-
-    #params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': path + "data"}}
-    #command_result = driver.execute("send_command", params)
-
     options = Options()
     options.headless = False
 
@@ -149,15 +115,8 @@ def process_file(file_src = ""):
 
             print(f'Row : {i}')
             
-            #if i == 200:
-                #wb.save('/home/edgar/Downloads/rica-docs/APAC_Chinese Posts_Translated.xlsx')
-                #break    
-
         except BaseException as e:
             print("ERROR: "+str(e))
-            #end_current_session(driver)
-            #wb.save('/home/edgar/Downloads/rica-docs/APAC_Chinese Posts_Translated.xlsx')
-            #process_file(s)
             continue
 
     file_name = os.path.basename(os.path.splitext(file_src)[0])
@@ -197,58 +156,6 @@ def SplitWords(src):
     except BaseException as e:
         print("ERROR: "+str(e))
     
-
-    ## Row = D (content) R (title)
-    #driver = start_new_session()
-    #driver.get("https://www.google.com/search?q=google+translate")
-    #ss = driver.find_element_by_css_selector('#tw-source-text-ta')
-    #try:
-        ##df = pd.read_csv(files,
-                        ##header=None,
-                        ##names=('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE'),
-                        ##compression='infer',
-                        ##error_bad_lines=False, warn_bad_lines=True)
-
-        #df = pd.read_csv(files)
-
-        #row = 1
-        #ws = wb.Book(files)
-        #while row <= len(df['B']):
-            #print(row)
-            #if str(df['E'][row]) == 'nan':
-                    #ss.clear()
-                    #ss = driver.find_element_by_css_selector('#tw-source-text-ta').send_keys(s)
-                    ## ss.send_keys(df['D'][row])
-                    #os.system("echo %s| clip" % str(df['D'][row]))
-                    #ss.send_keys(Keys.CONTROL, 'v')
-                    #time.sleep(2)
-                    #ss.click()
-                    #trsltd_c = str(driver.find_element_by_css_selector('#tw-target-text').text)
-                    ## print(trsltd)
-                    #ss.clear()
-                    #ss = driver.find_element_by_css_selector('#tw-source-text-ta')
-                    ## ss.send_keys(df['D'][row])
-                    #os.system("echo %s| clip" % str(df['S'][row]))
-                    #ss.send_keys(Keys.CONTROL, 'v')
-                    #time.sleep(2)
-                    #ss.click()
-                    #trsltd_t = str(driver.find_element_by_css_selector('#tw-target-text').text)
-                    #if trsltd_c == '':
-                            #trsltd_c = '.'
-                    #if trsltd_t == '':
-                            #trsltd_t = '.'
-                    #row = row + 1
-                    #wb.sheets['APAC_Chinese_Posts'+s].range((row,5)).value = trsltd_c
-                    #wb.sheets['APAC_Chinese_Posts'+s].range((row,20)).value = trsltd_t
-                    #wb.save()
-            #else:
-                    #row = row + 1
-    #except BaseException as e:
-        #print("ERROR: "+str(e))
-        #end_current_session(driver)
-        ##process_file(s)
-        #pass
-
 
 if __name__ == "__main__":   
     # with arguments
